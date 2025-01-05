399b23c8dcbb9ceed538d7c963bba2d693e94b68
Khadiza Sultana <sultanakhadiza37@gmail.com>
2025-01-05 12:12:41 +0600

    Update bookAllocationProblem.cpp

--- bookAllocationProblem.cpp
+++ bookAllocationProblem.cpp
@@ -2,47 +2,61 @@
 // Author: Khadiza Sultana
 // Date: 1/4/2025
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-bool isValid(vector<int>&arr, int n, int m, int maxAllowed) {
+bool isValid(vector<int> &arr, int n, int m, int maxAllowed)
+{
     int stu = 1, pages = 0;
-    for (int i = 0; i <n; i++) {
-        if (arr[i] > maxAllowed) return false;
-        if (pages+arr[i] <= maxAllowed) {
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > maxAllowed)
+            return false;
+        if (pages + arr[i] <= maxAllowed)
+        {
             pages += arr[i];
-        } else {
+        }
+        else
+        {
             stu++;
             pages = arr[i];
         }
     }
-    if (stu > m) return false;
-    else return true;
+    if (stu > m)
+        return false;
+    else
+        return true;
 }
 
-int allocateBooks(vector<int> &arr, int n, int m) {
+int allocateBooks(vector<int> &arr, int n, int m)
+{
     int sum = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         sum += arr[i];
     }
-    int st=0, end=sum, ans = 0; // range of possible answer
-    while (st <= end) {
-        int mid = st + (end-st)/2;
-        if (isValid(arr, n, m, mid)) { // valid ans so search left
+    int st = 0, end = sum, ans = 0; // range of possible answer
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+        if (isValid(arr, n, m, mid))
+        { // valid ans so search left
             ans = mid;
             end = mid - 1;
         }
-        else { // invalid so search right
+        else
+        { // invalid so search right
             st = mid + 1;
         }
     }
     return ans;
 }
 
-int main() {
-    vector<int>arr = {20, 10, 35, 62};
+int main()
+{
+    vector<int> arr = {20, 10, 35, 62};
     int n = 4, m = 2;
     cout << allocateBooks(arr, n, m) << endl;
     return 0;
