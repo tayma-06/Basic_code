33fac4a508d8b2a64e208b1b63849ebb646cf246
Khadiza Sultana <sultanakhadiza37@gmail.com>
2025-01-19 05:09:46 +0600

    Update 04CountDuplicate.c

--- C/Array/04CountDuplicate.c
+++ C/Array/04CountDuplicate.c
@@ -41,41 +41,4 @@ int main()
     printf("Count of Duplicate Elements in the Array : %d", countDuplicates(arr, n))arr, n));
 
     return 0;
-}  <stdio.h>
-
-void storeArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-}
-
-int countDuplicates(int arr[], int n)
-{
-    int count = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
-        {
-            if (arr[i] == arr[j++])
-                count++;
-        }
-    }
-    return count;
-}
-
-int main()
-{
-    int n;
-    printf("Enter the size of array : ");
-    scanf("%d", &n);
-    int arr[n];
-
-    printf("Enter the elements of the array: ");
-    storeArray(arr, n);
-
-    printf("Count of Duplicate Elements in the Array : %d", sumOfElements(arr, n));
-
-    return 0;
-}
+}  
