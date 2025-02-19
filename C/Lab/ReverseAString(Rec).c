7af77683c1bc5f56643622622dfa045e5cee2c71
Khadiza Sultana <sultanakhadiza37@gmail.com>
2025-02-19 17:53:45 +0600

    Update ReverseAString(Rec).c

--- C/Lab/ReverseAString(Rec).c
+++ C/Lab/ReverseAString(Rec).c
@@ -1,7 +1,9 @@
-#include<stdio.h>
-#include<string.h>
-void reverseString(char str[], int start, int end){
-    if (start >= end) {
+#include <stdio.h>
+#include <string.h>
+void reverseString(char str[], int start, int end)
+{
+    if (start >= end)
+    {
         return;
     }
     char temp = str[start];
@@ -9,11 +11,12 @@ void reverseString(char str[], int start, int end){
     str[end] = temp;
     reverseString(str, start + 1, end - 1);
 }
-int main(){
+int main()
+{
     char str[100];
     scanf("%99s", &str);
-    int len=strlen(str);
-    reverseString(str, 0, len-1);
+    int len = strlen(str);
+    reverseString(str, 0, len - 1);
     printf("%s\n", str);
     return 0;
 }
