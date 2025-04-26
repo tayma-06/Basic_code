9eabd1ae3a1e5df8b9959d13c7bc99d25203239a
Khadiza Sultana <sultanakhadiza37@gmail.com>
2025-04-26 13:50:21 +0600

    Add files via upload

---
+++ Java/Choices.java
@@ -0,0 +1,8 @@
+public class Choices {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("Is Java Fun? " + b1);
+        System.out.println("Is Fish Tasty? " + b2);
+    }
+}
