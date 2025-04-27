f0700ce0a834d18508cef548b7235261522fab35
Khadiza Sultana <sultanakhadiza37@gmail.com>
2025-04-27 22:37:17 +0600

    Add files via upload

---
+++ Java/Basics/DoubleType.java
@@ -0,0 +1,8 @@
+class DoubleType {
+    public static void main(String[] args) {
+        double pi = 3.141592653589793;
+        double an = 6.02214076e23;
+        System.out.println("Value of pi : " + pi);
+        System.out.println("Value of Avogadro's Number : " + an);
+    }
+}
---
+++ Java/Basics/FloatType.java
@@ -0,0 +1,8 @@
+class FloatType {
+    public static void main(String[] args) {
+        float pi = 3.1416f;
+        float gravity = 9.81f;
+        System.out.println("The value of pi : " + pi);
+        System.out.println("The value of gravity : " + gravity);
+    }
+}
